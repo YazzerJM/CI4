9bc83a318e3f87a717ab7ff27b5c8e3856b3acc5
Yasser Jimenez <yasserjimenezmartinez@hotmail.com>
2022-09-04 19:06:54 -0500

    Rutas

--- app/Controllers/Front/Home.php
+++ app/Controllers/Front/Home.php
@@ -1,15 +1,19 @@
 <?php
 
-namespace App\Controllers;
+namespace App\Controllers\Front;
 
 use App\Libraries\Codigo;
-
+use App\Controllers\BaseController;
+    
 class Home extends BaseController
 {
     public function index()
     {
+        return view('welcome_message');
+    }
+
+    public function sayHola(){
         $inst = new Codigo();
         $inst->sayHola();
-        return view('welcome_message');
     }
 }
