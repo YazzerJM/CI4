5f202ea4a14b6b1576773648c92b461bbe673ea9
Yasser Jimenez <yasserjimenezmartinez@hotmail.com>
2022-09-04 22:12:59 -0500

    layout

---
+++ app/Views/Front/layout/header.php
@@ -0,0 +1,10 @@
+<section class="hero is-info">
+    <div class="hero-body">
+        <p class="title">
+            Mi blog personal
+        </p>
+        <p class="subtitle">
+            Lista de entradas
+        </p>
+    </div>
+</section>
